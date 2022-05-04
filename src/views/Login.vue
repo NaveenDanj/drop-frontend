b0e9ca101d98b6afe42a7bf1c3e172f9f57d4182
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-05-05 01:21:07 +0530

    added login page

---
+++ src/views/Login.vue
@@ -0,0 +1,48 @@
+<template>
+    
+    <div style=" background-color : #1D1D1F; min-height : 100vh; overflow-y:hidden" class="d-flex justify-center align-center">
+        
+        <v-container class="" style="border: 1px solid #27272A; width : 30%; min-width : 400px; color : #A6A6A6"   >
+            
+            <div class="d-flex justify-center">
+                <img src="https://templates.iqonic.design/cloudbox/html/assets/images/logo-white.png"  style="width : 200px; " />
+            </div>
+
+            <div class="d-flex justify-center mt-5">
+                <h1>Sign In</h1>
+            </div>
+
+            <div class="d-flex justify-center mt-5">
+                <p>Login to stay connected.</p>
+            </div>
+
+            <v-form>
+
+                <v-text-field outlined dense placeholder="Email" color="#A6A6A6" dark/>
+                <v-text-field outlined dense placeholder="Password" color="#A6A6A6" dark/>
+
+                <div class="d-flex justify-space-between" >
+                    <!-- remember me radio button -->
+                    <v-checkbox label="Remember me" color="#A6A6A6" dark/>
+                    <label class="my-auto">Forgot Password</label>
+                </div>
+
+                <div class="d-flex justify-center" >
+                    <v-btn color="purple" rounded dark>Sign In</v-btn>
+                </div>
+
+                <div class="d-flex justify-center mt-5" >
+                    <label class="my-auto">Create an Account Sign Up</label>
+                </div>
+
+
+            </v-form>
+            
+
+        </v-container>
+
+
+    </div>
+
+
+</template>
