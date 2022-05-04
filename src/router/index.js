5d0d312a5b2c4bed13d1aa8506db7a96b0b70bf8
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-05-05 01:41:06 +0530

    added register page

--- src/router/index.js
+++ src/router/index.js
@@ -13,10 +13,12 @@ const routes = [
   {
     path: '/login',
     name: 'Login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Auth/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Auth/Register.vue')
   }
 ]
 
---
+++ src/views/Auth/Register.vue
@@ -0,0 +1,60 @@
+<template>
+    
+    <div style=" background-color : #1D1D1F; min-height : 100vh; overflow-y:hidden" class="d-flex justify-center align-center">
+        
+        <v-container class="" style="border: 1px solid #27272A; width : 30%; min-width : 400px; color : #A6A6A6"   >
+            
+            <div class="d-flex justify-center">
+                <img src="https://templates.iqonic.design/cloudbox/html/assets/images/logo-white.png"  style="width : 200px; " />
+            </div>
+
+            <div class="d-flex justify-center mt-5">
+                <h1>Sign Up</h1>
+            </div>
+
+            <div class="d-flex justify-center mt-5">
+                <p>Create your account.</p>
+            </div>
+
+            <v-form>
+
+                <v-text-field outlined dense placeholder="Display Name" color="#A6A6A6" dark/>
+                <v-text-field outlined dense placeholder="Email" color="#A6A6A6" dark/>
+
+                <v-row>
+
+                    <v-col cols="12" md="6" >
+                        <v-text-field outlined dense placeholder="Password" color="#A6A6A6" dark/>
+                    </v-col>
+
+                    <v-col cols="12" md="6">
+                        <v-text-field outlined dense placeholder="Confirm Password" color="#A6A6A6" dark/>
+                    </v-col>
+
+                </v-row>
+
+                <!-- agreement  -->
+                <div class="">
+                    <v-checkbox label="I agree to the terms and conditions" color="#A6A6A6" dark/>
+                </div>
+
+
+                <div class="d-flex justify-center" >
+                    <v-btn color="purple" rounded dark>Sign Up</v-btn>
+                </div>
+
+                <div class="d-flex justify-center mt-5" >
+                    <label class="my-auto">Already have an Account Sign In</label>
+                </div>
+
+
+            </v-form>
+            
+
+        </v-container>
+
+
+    </div>
+
+
+</template>
