1f7cc568bc47eea75f00a4f9ebea167d87ee3444
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-05-20 00:20:05 +0530

    added small ui changes to login and register components

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -2,7 +2,7 @@
     
     <div style=" background-color : #1D1D1F; min-height : 100vh; overflow-y:hidden" class="d-flex justify-center align-center">
         
-        <v-container class="" style="border: 1px solid #27272A; width : 30%; min-width : 400px; color : #A6A6A6"   >
+        <v-container class="" style="border: 1px solid #27272A; width : 30%; min-width : 350px; color : #A6A6A6"   >
             
             <div class="d-flex justify-center">
                 <img src="https://templates.iqonic.design/cloudbox/html/assets/images/logo-white.png"  style="width : 200px; " />
@@ -32,7 +32,7 @@
                 </div>
 
                 <div class="d-flex justify-center mt-5" >
-                    <label class="my-auto">Create an Account Sign Up</label>
+                    <label class="my-auto">Create an Account</label> <label class="ml-1 blue--text" @click="() => $router.push('/register')">Sign Up</label>
                 </div>
 
 
--- src/views/Auth/Register.vue
+++ src/views/Auth/Register.vue
@@ -2,7 +2,7 @@
     
     <div style=" background-color : #1D1D1F; min-height : 100vh; overflow-y:hidden" class="d-flex justify-center align-center">
         
-        <v-container class="" style="border: 1px solid #27272A; width : 30%; min-width : 400px; color : #A6A6A6"   >
+        <v-container class="" style="border: 1px solid #27272A; width : 30%; min-width : 350px; color : #A6A6A6"   >
             
             <div class="d-flex justify-center">
                 <img src="https://templates.iqonic.design/cloudbox/html/assets/images/logo-white.png"  style="width : 200px; " />
@@ -44,7 +44,7 @@
                 </div>
 
                 <div class="d-flex justify-center mt-5" >
-                    <label class="my-auto">Already have an Account Sign In</label>
+                    <label class="my-auto">Already have an Account? </label> <label class="ml-1 blue--text" @click="() => $router.push('/login')">Sign In</label>
                 </div>
 
 
