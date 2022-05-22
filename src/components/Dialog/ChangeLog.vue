ddd665309981d26beab20c3ccc9129bdd6d342cb
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-05-22 12:34:27 +0530

    added small changes

--- src/components/Dialog/ChangeLog.vue
+++ src/components/Dialog/ChangeLog.vue
@@ -20,28 +20,36 @@
 
       <v-card-text class="pa-3" style="height : 400px;">
         <!-- drag and drop file or click to open file dialog -->
-        <div class="ma-2">
-            <label >Update 1.1.0: The update addresses this security issue.</label><br/>
-            <label>Release Date : 28/05/2022 </label><br/>
-        </div>
-        
-        <v-divider></v-divider>
-        
-        <div class="pa-2">
-            <p>Welcome to the May 2022 release of DROP. There are many updates in this version that we hope you'll like, some of the key highlights include:</p>
-        </div>
 
         <div>
 
-            <ul>
-                <li>Add new Dashboard to registered users.</li>
-                <li>Registered users can upload permeant files.</li>
-                <li>Registered users can see their uploaded files.</li>
-                <li>Add page to see release logs / release notes.</li>
-                <li>Added 404 page.</li>
-            </ul>
+            <div class="ma-2">
+                <label >Update 1.1.0: The update addresses this security issue.</label><br/>
+                <label>Release Date : 28/05/2022 </label><br/>
+            </div>
+            
+            <v-divider></v-divider>
+            
+            <div class="pa-2">
+                <p>Welcome to the May 2022 release of DROP. There are many updates in this version that we hope you'll like, some of the key highlights include:</p>
+            </div>
+
+            <div>
+
+                <ul>
+                    <li>Add new Dashboard to registered users.</li>
+                    <li>Registered users can upload permeant files.</li>
+                    <li>Registered users can see their uploaded files.</li>
+                    <li>Add page to see release logs / release notes.</li>
+                    <li>Added 404 page.</li>
+                </ul>
+
+            </div>
+
 
         </div>
+        
+        
 
       </v-card-text>
 
