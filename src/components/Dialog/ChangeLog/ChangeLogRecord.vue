1b9e5cf9b79d8ebecfeaf52b685c8d28383945a7
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-05-22 14:04:21 +0530

    added chaneg log record component

---
+++ src/components/Dialog/ChangeLog/ChangeLogRecord.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="pa-2">
+    <div class="ma-2">
+      <label
+        ><b>Update 1.1.0: The update addresses this security issue.</b></label
+      ><br />
+      <label><b>Release Date : 28/05/2022 </b></label><br />
+    </div>
+
+    <v-divider></v-divider>
+
+    <div class="pa-2">
+      <p>
+        Welcome to the May 2022 release of DROP. There are many updates in this
+        version that we hope you'll like, some of the key highlights include:
+      </p>
+    </div>
+
+    <div>
+      <ul>
+        <li>Add new Dashboard to registered users.</li>
+        <li>Registered users can upload permeant files.</li>
+        <li>Registered users can see their uploaded files.</li>
+        <li>Add page to see release logs / release notes.</li>
+        <li>Added 404 page.</li>
+      </ul>
+    </div>
+
+    <v-divider></v-divider>
+  </div>
+</template>
