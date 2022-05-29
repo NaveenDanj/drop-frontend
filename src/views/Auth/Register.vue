87f3b9c09620e090c7c24d48d48615be7d441bcc
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-05-29 22:54:51 +0530

    added form validation to register form

--- src/views/Auth/Register.vue
+++ src/views/Auth/Register.vue
@@ -16,19 +16,66 @@
                 <p>Create your account.</p>
             </div>
 
-            <v-form>
+            <v-form ref="registerForm" @submit="submit">
 
-                <v-text-field outlined dense placeholder="Display Name" color="#A6A6A6" dark/>
-                <v-text-field outlined dense placeholder="Email" color="#A6A6A6" dark/>
+                <v-text-field 
+                    v-model="form.name"
+                    outlined
+                    dense
+                    placeholder="Display Name"
+                    color="#A6A6A6"
+                    dark
+                    :rules="[
+                        v => !!v || 'Name is required',
+                        v => v.length <= 20 || 'Name must be less than 20 characters'
+                    ]"
+                />
+
+                <v-text-field
+                    v-model="form.email"
+                    outlined
+                    dense
+                    placeholder="Email"
+                    color="#A6A6A6"
+                    dark
+                    :rules="[
+                        v => !!v || 'Email is required',
+                        v => /.+@.+\..+/.test(v) || 'Email must be valid'
+                    ]"
+                />
 
                 <v-row>
 
                     <v-col cols="12" md="6" >
-                        <v-text-field outlined dense placeholder="Password" color="#A6A6A6" dark/>
+                        <v-text-field
+                            type="password"
+                            v-model="form.password"
+                            outlined
+                            dense
+                            placeholder="Password"
+                            color="#A6A6A6"
+                            dark
+                            :rules="[
+                                v => !!v || 'Password is required',
+                                v => v.length >= 6 || 'Password must be at least 6 characters'
+                            ]"
+                        />
                     </v-col>
 
                     <v-col cols="12" md="6">
-                        <v-text-field outlined dense placeholder="Confirm Password" color="#A6A6A6" dark/>
+                        <v-text-field
+                            type="password"
+                            v-model="form.password_confirmation"
+                            outlined
+                            dense
+                            placeholder="Confirm Password"
+                            color="#A6A6A6"
+                            dark
+                            :rules="[
+                                v => !!v || 'Password confirmation is required',
+                                v => v === form.password || 'Password confirmation must match password'
+                            ]"
+                        />
                     </v-col>
 
                 </v-row>
@@ -40,7 +87,7 @@
 
 
                 <div class="d-flex justify-center" >
-                    <v-btn color="purple" rounded dark>Sign Up</v-btn>
+                    <v-btn type="submit" color="purple" rounded dark>Sign Up</v-btn>
                 </div>
 
                 <div class="d-flex justify-center mt-5" >
@@ -58,3 +105,33 @@
 
 
 </template>
+
+<script>
+export default {
+    
+
+    data(){
+        return {
+            form : {
+                name : '',
+                email : '',
+                password : '',
+                password_confirmation : ''
+            },
+
+            error : null
+        }
+    },
+
+    methods : {
+
+        submit(e){
+            e.preventDefault();
+            console.log('submit');
+        }
+
+    }
+
+
+}
+</script>
